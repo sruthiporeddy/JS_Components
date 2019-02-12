6aa87ba46e45befd0c08f6e940913721d7500a8e
sruthiporeddy <45047403+sruthiporeddy@users.noreply.github.com>
2019-02-11 20:58:22 -0500

    Add files via upload
    Template added

---
+++ new.js
@@ -0,0 +1,7 @@
+// const toggleMenu = document.querySelector('.toggle')
+// toggleMenu.addEventListener('click',function(){
+//     console.log('hello');
+//     const menuClass = document.querySelector('nav ul');
+//     menuClass.classList.toggle('open');
+
+// })
